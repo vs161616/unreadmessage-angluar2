05f535a192090173b44f1f052ccbb6a3a68e1377
vs161616 <vs161616@gmail.com>
2021-04-17 18:02:03 +0530

    3 files modified

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,23 +1,28 @@
-import { Route } from '@angular/compiler/src/core';
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { MessageServiceService } from '../message-service.service';
+import { Route } from "@angular/compiler/src/core";
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { MessageServiceService } from "../message-service.service";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.css"]
 })
 export class HeaderComponent implements OnInit {
-
-  constructor(private messageService:MessageServiceService, private router:Router) { }
-  counter:any= 3
-  ngOnInit(): void {
-  }
-  sendmessage(event:any){ 
-      this.messageService.messageData.next(1);
-      this.router.navigateByUrl('/home')
-      this.counter = 0
-      
+  constructor(
+    private messageService: MessageServiceService,
+    private router: Router
+  ) {}
+  counter: any = 3;
+  ngOnInit(): void {}
+  sendmessage(event: any) {
+    if (event.target) {
+      this.messageService.send(1);
+      // this.messageService.messageData.next(1);
+      this.router.navigateByUrl("/home");
+      this.counter = 0;
+    } else {
+      this.counter = 3;
     }
+  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,30 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import { MessageServiceService } from '../message-service.service';
+import { Component, OnInit } from "@angular/core";
+import { MessageServiceService } from "../message-service.service";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"]
 })
 export class HomeComponent implements OnInit {
   message: any;
+  subscription: any;
 
-  constructor(private messageService:MessageServiceService) { }
+  constructor(private messageService: MessageServiceService) {}
 
   ngOnInit(): void {
-    this.message  = {
-      "message1":"Hello  this side alexender !!",
-      "message2":"How Are You ??",
-      "message3":"I am fine"
-    }
-    this.messageService.messageData.subscribe(
-      (message) => {
-        if(message==1){
-         
-        }
-        
+    // this.message = {
+    //   message1: "Hello  this side alexender !!",
+    //   message2: "How Are You ??",
+    //   message3: "I am fine"
+    // };
+    this.subscription = this.messageService.getMessage().subscribe(message => {
+      if (message) {
+        this.message = message;
+        console.log(this.message);
       }
-    );
+    });
   }
-
 }
